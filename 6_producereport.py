bde0e6f1c699eeda8b9f2f3c7e7a5ae18bede75f
Brinton Mucher <brinton_mucher1@baylor.edu>
2026-02-17 16:24:07 -0600

    Finished produce

--- 6_producereport.py
+++ 6_producereport.py
@@ -207,3 +207,15 @@ ProduceDictionary={
     }
 }
 
+for item, details in ProduceDictionary.items():
+    cost = details['cost']
+    amt_sold = details['amt_sold']
+    stated_total = details['total']
+    
+    calculated_total = round(cost * amt_sold, 2)
+    
+    if calculated_total != stated_total:
+        print(f"{item}")
+        print(f"  Stated Total: ${stated_total}")
+        print(f"  Calculated Total: ${calculated_total}")
+        print()
